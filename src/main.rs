3357d78ef505b8f598de6c51a0c1ee76023f7c73
Solomon Adams <sadams2013@gmail.com>
2022-02-20 11:05:16 -0500

    argparse -> clap

--- src/main.rs
+++ src/main.rs
@@ -9,89 +9,74 @@ pub mod utils;
 pub mod variants;
 pub mod statistics;
 
-use argparse::{ArgumentParser, StoreTrue, Store};
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about = "Find genetic interactions from GWAS-scale data", long_about = None)]
+struct Args {
+    #[clap(long, help="Provide more verbose output")]
+    verbose: bool,
+    #[clap(short, long, help="Path to input file")]
+    file_path: String,
+    #[clap(long, help="Path to file with variant names.")]
+    variant_file_path: String,
+    #[clap(long, help="Number of trees in selection forest.")]
+    n_tree: i32,
+    #[clap(long, help="MTRY fraction for selection forest.")]
+    mtry: f64,
+    #[clap(long, help="Max depth for selection forest.")]
+    max_depth: i32,
+    #[clap(long, help="Subject fraction for selection forest.")]
+    subj_fraction: f64,
+    #[clap(long, help="Number of trees for iterative forest.")]
+    n_tree_2: i32,
+    #[clap(long, help="MTRY fraction for iterative forest.")]
+    mtry_2: f64,
+    #[clap(long, help="Max depth for iterative forest.")]
+    max_depth_2: i32,
+    #[clap(long, help="Subject fraction for iterative forest.")]
+    subj_fraction_2: f64,
+    #[clap(long, help="Number of iterations for iterative forest.")]
+    n_iter: usize,
+    #[clap(long, help="Z score to keep variants after selection forest.")]
+    z_keep: f64,
+    #[clap(long, help="Outcome is a continuous variable.")]
+    continuous_outcome: bool,
+    #[clap(long, help="Write forest to stdout (very verbose output)")]
+    output_forest: bool,
+    #[clap(short, long, help="Number of threads to use.")]
+    threads: usize
+}
 
 
 fn main() {
-    let mut verbose: bool = false;
-    let mut file_path: String = "".to_string();
-    let mut variant_file_path: String = "".to_string();
-    let mut n_tree: i32 = 10;
-    let mut mtry: f64 = 0.333;
-    let mut max_depth: i32 = 5;
-    let mut subj_fraction: f64 = 0.666;
-    let mut n_tree_2: i32 = 10;
-    let mut mtry_2: f64 = 0.333;
-    let mut max_depth_2: i32 = 5;
-    let mut subj_fraction_2: f64 = 0.666;
-    let mut n_iter: usize = 1; 
-    let mut z_keep: f64 = 1.0;
-    let mut continuous_outcome: bool = false;
-    let mut output_forest: bool = false;
-    let mut threads: usize = 1;
-    {
-        let mut ap = ArgumentParser::new();
-        ap.set_description("Find genetic interactions from GWAS-scale data");
-        ap.refer(&mut verbose)
-        .add_option(&["-v", "--verbose"], StoreTrue,"Provide more verbose output");
-        ap.refer(&mut file_path)
-        .add_option(&["-f", "--file-path"], Store, "Path to input file.");
-        ap.refer(&mut variant_file_path)
-        .add_option(&["-x", "--variant-file-path"], Store, "Path to file with variant IDs");
-        ap.refer(&mut n_tree)
-        .add_option(&["--num-tree"], Store, "Number of trees to train.");
-        ap.refer(&mut mtry)
-        .add_option(&["--mtry-fraction"], Store, "MTRY fraction.");
-        ap.refer(&mut max_depth)
-        .add_option(&["--max-depth"], Store, "Max tree depth.");
-        ap.refer(&mut subj_fraction)
-        .add_option(&["--subject-fraction"], Store, "Fraction of subjects in each random sample.");
-        ap.refer(&mut n_tree_2)
-        .add_option(&["--num-tree-2"], Store, "Number of trees to train.");
-        ap.refer(&mut mtry_2)
-        .add_option(&["--mtry-fraction-2"], Store, "MTRY fraction.");
-        ap.refer(&mut max_depth_2)
-        .add_option(&["--max-depth-2"], Store, "Max tree depth.");
-        ap.refer(&mut subj_fraction_2)
-        .add_option(&["--subject-fraction-2"], Store, "Fraction of subjects in each random sample.");
-        ap.refer(&mut n_iter)
-        .add_option(&["-r", "--iterations"], Store, "Number of iterations of the genetic algorithm.");
-        ap.refer(&mut z_keep)
-        .add_option(&["-z", "--z-score-cutoff"], Store, "Keep variants with Z score above.");
-        ap.refer(&mut threads)
-        .add_option(&["-t", "--threads"], Store, "Number of threads to use.");
-        ap.refer(&mut output_forest)
-        .add_option(&["--output-forest"], StoreTrue, "Output forest");
-        ap.refer(&mut continuous_outcome)
-        .add_option(&["--continuous-outcome"], StoreTrue, "Outcome is continuous (vs binary)");
-        ap.parse_args_or_exit();
-    }
-    let filetype: u8 = input_file_type(&file_path);
+    let args = Args::parse();
+    let filetype: u8 = input_file_type(&args.file_path);
     let mut data = match filetype {
-        1 => reader::read_matrix_csv(&file_path, &",", &continuous_outcome),
-        2 => reader::read_matrix_csv(&file_path, &"\t", &continuous_outcome),
+        1 => reader::read_matrix_csv(&args.file_path, &",", &args.continuous_outcome),
+        2 => reader::read_matrix_csv(&args.file_path, &"\t", &args.continuous_outcome),
         _ => panic!("Filetype not supported!"),
     };
     let variants = match filetype {
-        1 => reader::read_variant_table(&variant_file_path, &",", &data.n_genotypes),
-        2 => reader::read_variant_table(&variant_file_path, &"\t", &data.n_genotypes),
+        1 => reader::read_variant_table(&args.variant_file_path, &",", &data.n_genotypes),
+        2 => reader::read_variant_table(&args.variant_file_path, &"\t", &data.n_genotypes),
         _ => panic!("Filetype not supported!"),
     };
-    utils::make_thread_pool(threads);
+    utils::make_thread_pool(args.threads);
     eprintln!("Growing initial forest");
     let hp = forest::HyperParameters {
-        n_tree: n_tree, 
-        mtry: mtry, 
-        max_depth: max_depth, 
-        subj_fraction: subj_fraction,
-        continuous_outcome: continuous_outcome
+        n_tree: args.n_tree, 
+        mtry: args.mtry, 
+        max_depth: args.max_depth, 
+        subj_fraction: args.subj_fraction,
+        continuous_outcome: args.continuous_outcome
     };
     let hp2 = forest::HyperParameters {
-        n_tree: n_tree_2, 
-        mtry: mtry_2, 
-        max_depth: max_depth_2, 
-        subj_fraction: subj_fraction_2,
-        continuous_outcome: continuous_outcome
+        n_tree: args.n_tree_2, 
+        mtry: args.mtry_2, 
+        max_depth: args.max_depth_2, 
+        subj_fraction: args.subj_fraction_2,
+        continuous_outcome: args.continuous_outcome
     };
     let mut f = forest::Forest::new(hp);
     match f.grow(&data) {
@@ -102,12 +87,12 @@ fn main() {
         }
     }
     f.print_var_importance(&variants);
-    let k_vars = f.keep_vars(z_keep);
+    let k_vars = f.keep_vars(args.z_keep);
     eprintln!("Keeping {:?} variants and initiating iterative grow and prune.", &k_vars.len());
     &data.set_genotype_indices(k_vars);
     f.update_hyperparameters(hp2);
-    for n in 1..n_iter + 1 {
-        eprintln!("Growing forest {:?} of {:?}", n, n_iter);
+    for n in 1..&args.n_iter + 1 {
+        eprintln!("Growing forest {:?} of {:?}", n, &args.n_iter);
         match f.grow(&data) {
             Ok(_) => (),
             Err(err) => {
@@ -118,7 +103,7 @@ fn main() {
         println!("## ITERATION: {}", n);
         println!("#OVERALL_IMPORTANCE");
         f.print_var_importance(&variants);
-        if output_forest {
+        if args.output_forest {
             //Output trees
             for tree in f.trees.as_ref().unwrap() {
                 println!("#TREE");
